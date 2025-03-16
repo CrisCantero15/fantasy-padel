aa24e7f39f4040b5db0b4dabf05117004400cbab
CrisCantero15 <cristiancanterolopez@gmail.com>
2025-03-16 21:10:00 +0100

    Finalizada la sección de Contacto.php

--- controllers/ContactoControlador.php
+++ controllers/ContactoControlador.php
@@ -14,6 +14,27 @@ class ContactoControlador {
 
     }
 
+    public function enviarCorreo() {
+        
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+            if (isset($_POST["nombre"]) && !empty($_POST["nombre"]) && isset($_POST["email"]) && !empty($_POST["email"]) && isset($_POST["mensaje"]) && !empty($_POST["mensaje"])) {
+
+                $nombre = $_POST["nombre"];
+                $email = $_POST["email"];
+                $mensaje = $_POST["mensaje"];
+
+            } else {
+                    
+                    $data["errorEnvio"] = "Faltan campos por rellenar";
+                    require_once "views/Vista.php";
+                    $vista = new Vista();
+                    $vista->renderizarVista("contacto", $data);
+                
+            }
+    
+        }
+    }
 }
 
 ?>
--- views/contacto.php
+++ views/contacto.php
@@ -4,10 +4,31 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Fantasy Padel - Contacto</title>
+    <link rel="stylesheet" href="../assets/css/styles.css">
 </head>
 <body>
-    <p>¡Bienvenido a Fantasy Padel!</p>
-    <p>Estoy en CONTACTO</p>
-    <!-- Maquetar sección de registro de un nuevo usuario -->
+    <!-- Añadir el header aquí -->
+    <main>
+        <section>
+            <div class="form-container">             
+                <div class="text-form">
+                    <h1>Contacto</h1>
+                    <p>Si tienes alguna incidencia o sugerencia, por favor, escríbenos</p>
+                </div>
+                <form action="<?php echo $rutaApp ?>contacto/enviarCorreo" method="POST">
+                    <input type="text" id="nombre" name="nombre" placeholder="Nombre">
+                    <input type="email" id="email" name="email" placeholder="Email">
+                    <textarea id="mensaje" name="mensaje" placeholder="Escribe tu incidencia..."></textarea>
+                    <?php if (isset($data["errorEnvio"])) { ?>
+                    <div class="alert-text">
+                        <p><?php echo $data["errorEnvio"] ?></p>
+                    </div>
+                    <?php } ?> 
+                    <button type="submit">Enviar</button>
+                </form>
+            </div>
+        </section>
+    </main>
+    <!-- Añadir el footer aquí -->
 </body>
 </html>
